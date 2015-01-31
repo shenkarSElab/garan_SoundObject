68690f9f85bbae42a482529147214c1484d1c052
yair <yair99@gmail.com>
2015-01-31 22:37:15 +0200

    removed garan lib , integrated into the ino file.
    now add the accel lib

--- garan_SoundObject.ino
+++ garan_SoundObject.ino
@@ -1,106 +1,70 @@
 
-#include <Arduino.h>
 #include <SoftwareSerial.h>
-#include "Garan.h"
-#include <Wire.h>
-#include "Adafruit_LSM303.h"
 
-#if !defined(__MSP430_CPU__)
-SoftwareSerial garanSerial(8, 9); // RX, TX
-#endif
+SoftwareSerial mySerial(10, 11); // RX, TX
 
-Garan player(garanSerial);
-//A4(SDA) A5(SCL)
-Adafruit_LSM303 lsm;
+const char singleplay[]={0x24, 0x07, 0x06, 0x01, 0x31, 0x2E, 0x6D, 0x70, 0x33, 0x31};
+const char stopy[]={0x24, 0x02, 0x07, 0x00 ,0x05};
+unsigned char commandBuff[13];
 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("restart");
 
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600); 
-  // Try to initialise and warn if we couldn't detect the chip
-  if (!lsm.begin())
-  {
-    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
-    while (1);
+    
+    mySerial.begin(9600);
+    commandBuff[2] = 1;
 
-  }
-
-
-  Serial.println("Console is ready!");
-  Play (); 
-} 
-
-void loop() {
-
-  Accelo();
-  int volXval = ((int)lsm.accelData.x);
-  int outputXValue = map(volXval, 1024,-1024, 0, 15); 
-  player.setVolume(outputXValue);
-  playermenu () ;
+    singleLoopPlay(1);  delay(10000);
+ /*
+ for(int iii=15; iii>=0; iii--){
+    setVolume(iii);
+    delay(500);
+ }
+*/
 
 }
-
-void Play (){
-
-  if (garanSerial.available()) {   
-    unsigned char recvByte = garanSerial.read();
-    //  if (recvByte == 0x24) Serial.println();
-
-    //    Serial.print(String(recvByte, HEX) + " ");
-  }
-
-  player.singleLoopName("1.mp3");
-
+void loop(){
+    setVolume(random(15));
+    delay(500);
 }
 
-void Accelo (){
-  lsm.read();
-  Serial.print("X: "); 
-  Serial.print((int)lsm.accelData.x); 
-  Serial.print(" ");
-  Serial.print("Y: "); 
-  Serial.print((int)lsm.accelData.y);     
-  Serial.print(" ");
-  Serial.print("Z: "); 
-  Serial.println((int)lsm.accelData.z);   
-  Serial.print(" ");
 
-}
+void sendCommand(uint8_t command[])  {
+    uint8_t checksum = 0;
 
-void  playermenu (){
-
-  if (Serial.available()) {  
-
-    switch(Serial.read()) {
-
-    case '4':
-      player.singlePlayName("1.mp3");
-      break;
-    case '5':
-      player.sequencePlayName("1.mp3");
-      break;
-    case '6':
-      player.singleLoopName("1.mp3");
-      break;
-    case '9':
-      player.setVolume(15);
-      break;
-    case '0':
-      player.setVolume(0);
-      break;      
-    case 'f':
-      player.feedbackAtEnd();
-      break;
-    case ']':
-      player.volumeUp();
-      break;
-    case '[':
-      player.volumeDown();
-      break; 
-    default:
-      Serial.println("This key maps to none.");     
+    mySerial.write(0x24);
+    for (uint16_t i = 0; i <= command[0]; i++) {
+        mySerial.write(command[i]);
+        checksum ^= command[i];
     }
-  }
 
+    mySerial.write(checksum);
+}
+
+void buildHead(uint8_t len, uint8_t cmd)  {
+    commandBuff[0] = len;
+    commandBuff[1] = cmd;
+}
+
+void singleLoopPlay(uint16_t number)  {
+    buildHead(0x04, 0x02);
+    commandBuff[3] = (uint8_t)(number >> 8);
+    commandBuff[4] = (uint8_t)number;
+
+    sendCommand(commandBuff);
+}
+
+void stop()  {
+    mySerial.write(stopy,sizeof(stopy));
+    //sendCommand((uint8_t *)STOP);
+}
+
+void setVolume(uint8_t volume)  {
+    buildHead(0x03, 0x0D);
+    commandBuff[3] = volume;
+
+    sendCommand(commandBuff);
 }
 
